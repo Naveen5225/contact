d71f13c32f9fd1e9d8d2f4a6ff8a769278ded8fc
Naveen5225 <65850894+Naveen5225@users.noreply.github.com>
2021-05-13 16:25:06 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $to = "naveenkumarnn223@mail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
